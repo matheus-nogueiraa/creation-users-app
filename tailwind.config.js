250c8b2c1502164b7e95f4d3e864814329a59def
matheus-nogueira <matheus.vs.br@gmail.com>
2025-03-05 19:55:12 -0300

    first commit: start login page

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        light: '#40CFFF',
+        default: '#00BCFF',
+        dark: '#1D232A',
+        darkest: '#0B0D11',
+      },
+    },
+  },
+  plugins: [],
+}
